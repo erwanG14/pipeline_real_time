b976f3e378924e39769f886115e8e5a006811145
erwanG14 <erwan.germain14@gmail.com>
2026-01-15 15:01:41 +0100

    nasa request

---
+++ api/nasa_request.py
@@ -0,0 +1,10 @@
+import requests
+try:
+    f = open(r"C:\perso\nasa_api_key\api_keys.txt")
+    api_key = f.read()
+except : 
+    api_key = 'your_nasa_api_keys'
+url = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key="+api_key
+result = requests.get("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=CIM9MbRo4JP9Cge440Z5YVBsSqgacL1hNkLInc3c")
+data = result.json()
+print(data["near_earth_objects"][1])
